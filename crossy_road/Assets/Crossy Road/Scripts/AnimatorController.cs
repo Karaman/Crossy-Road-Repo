57ecc742fb4320d0be1cb90cb637d51b641e4713
Karaman <43113389+Karaman@users.noreply.github.com>
2018-09-09 18:51:16 +0200

    crossy road game tutorial repo
    crossy road game tutorial repository

---
+++ crossy_road/Assets/Crossy Road/Scripts/AnimatorController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorController : MonoBehaviour
+{
+    public PlayerController PlayerController = null;
+    private Animator animator = null;
+
+    void Start()
+    {
+        animator = this.GetComponent<Animator>(); 
+    }
+
+    void Update()
+    {
+        if (PlayerController.isDead)
+        {
+            animator.SetBool("Dead", true); 
+        }
+        if (PlayerController.jumpStart) 
+        {
+            animator.SetBool("JumpStart", true); 
+        }
+        else if (PlayerController.isJumping)
+        {
+            animator.SetBool("Jump", true);
+        }
+        else
+        {
+            animator.SetBool("Jump", false);
+            animator.SetBool("JumpStart", false);
+        }
+        if (!PlayerController.isIdle) return;
+        if (Input.GetKeyDown(KeyCode.UpArrow   )) { PlayerController.transform.rotation = Quaternion.Euler(0, 0, 0); }
+        if (Input.GetKeyDown(KeyCode.DownArrow )) { PlayerController.transform.rotation = Quaternion.Euler(0, 180, 0); }
+        if (Input.GetKeyDown(KeyCode.LeftArrow) ) { PlayerController.transform.rotation = Quaternion.Euler(0, -90, 0); }
+        if (Input.GetKeyDown(KeyCode.RightArrow)) { PlayerController.transform.rotation = Quaternion.Euler(0, 90, 0); }
+    }
+
+}
---
+++ crossy_road/Assets/Crossy Road/Scripts/Movers.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movers : MonoBehaviour
+{
+    public float speed             =1.0f;
+    public float moveDirection     = 0  ;
+    public bool parentOnTrigger   = true;
+    public bool hitBoxOnTrigger  = false;
+    public GameObject moverObject = null;
+
+    private Renderer renderer = null;
+    private bool isVisible  = false;
+
+    private void Start()
+    {
+        renderer = moverObject.GetComponent<Renderer>(); 
+    }
+    private void Update()
+    {
+        this.transform.Translate(speed * Time.deltaTime, 0, 0); 
+        IsVisible(); 
+
+    }
+    void IsVisible ()
+    {
+        if (renderer.isVisible)
+        {
+            isVisible  = true; 
+        }
+        if (!renderer.isVisible && isVisible )
+        {
+            Debug.Log("Remove object. Object is no longer seen by camera! ");
+            Destroy(this.gameObject); 
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Enter"); 
+            if (parentOnTrigger)
+            {
+                Debug.Log("Enter: Parent to me");
+                other.transform.parent = this.transform; 
+            }
+            if (hitBoxOnTrigger)
+            {
+                Debug.Log("Enter: Gothit. Game Over!");
+                other.GetComponent<PlayerController>().GotHit(); 
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if(parentOnTrigger)
+            {
+                other.transform.parent = null;
+                Debug.Log("Exit");
+            }
+            
+        }
+    }
+
+}
